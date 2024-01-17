c8f2b40383c2cbdbf65d9fdf06c3a2de89e1d982
a9202507 <a9202507@gmail.com>
2024-01-17 17:04:48 +0800

    Add files via upload

--- raffle.js
+++ raffle.js
@@ -56,11 +56,21 @@ function csvToArray(csvString, isGift) {
     } catch (error) {
       // Log the error with the line number. Note that index starts from 0, so add 1 to represent the actual line number.
       //console.error(`Error in line ${index + 2}: ${row}. Error message: ${error.message}`);
-      theErrorMessage = `csv檔異常，請確認人員.csv 姓名欄位第 ${
+
+      const errorModalElement = document.getElementById("errorModal"); //綁定index.html下的errorModal元件
+      const errorModalMessageElement =
+        document.getElementById("errorModalMessage"); //綁定index.html下的errorModalMessage元件
+
+      //設置彈出視窗的錯誤訊息
+
+      errorModalMessageElement.textContent = `csv檔異常，請確認人員.csv第一行欄位數量至少五欄。也請檢查第 ${
         index + 2
       }行的資料，是否有空白欄位存在.`;
 
-      raffleCloseMessage(".CSV檔案異常", theErrorMessage);
+      const errorModal = new bootstrap.Modal(errorModalElement); //顯示錯誤彈出視窗
+      errorModal.show();
+      // Return null or an error object for that row
+      return { error: `Line ${index + 2}: ${error.message}` };
     }
   });
 
@@ -85,13 +95,7 @@ function updateEligibleParticipantsList() {
   });
 
   document.getElementById("totalParticipants").textContent = total; //將參加人數投放到網頁
-  // 調用函數以查找和顯示重複項
-
-  if (total === 0) {
-    raffleCloseMessage("抽獎結束", "可抽獎人員不足");
-    document.getElementById("startRaffle-button").disabled = true; // 關閉抽獎跟洗牌按鈕
-  }
-
+  // 调用函数以查找和显示重复项
   findDuplicates();
 }
 //更新可抽禮物清單到網頁
@@ -108,8 +112,6 @@ function updateAvailablePrizesList() {
       listItem.textContent = `${gift.name} (剩餘數量: ${gift.quantity})`; //設定列表內容
       list.appendChild(listItem); //將名單渲染到網頁上
       gift_total = gift_total + gift.quantity; //計算總共可抽數量
-    } else {
-      raffleCloseMessage("抽獎完成", "可用禮物數量不足，抽獎結束");
     }
   });
   document.getElementById("totalPrizes").textContent = gift_total; //將可抽數量渲染到網頁上
@@ -121,7 +123,7 @@ function updateWinnersList() {
     .getElementById("winnersList")
     .querySelector("tbody"); //將網頁元件winnersList下的tbody表格綁定到此
 
-  tableBody.innerHTML = ""; //重設此html網頁元件
+  tableBody.innerHTML = ""; //重置此html網頁元件
   winners.forEach((winner) => {
     //開始迖代
     const row = tableBody.insertRow(); //在html元件插入一行
@@ -141,17 +143,26 @@ function updateWinnersList() {
 function startRaffle() {
   //準備開始抽獎
   //檢查是否上傳人員.csv  獎品.csv
+  if (participants.length === 0 || gifts.length === 0) {
+    //若有一個名單沒上傳，則準備錯誤訊息
+    const errorModalElement = document.getElementById("errorModal"); //綁定index.html下的errorModal元件
+    const errorModalMessageElement =
+      document.getElementById("errorModalMessage"); //綁定index.html下的errorModalMessage元件
 
-  //設置彈出視窗的錯誤訊息
-  if (participants.length === 0 && gifts.length === 0) {
-    raffleCloseMessage("錯誤", "無法執行抽獎，需先上傳人員名單和獎品清單。");
-  } else if (participants.length === 0) {
-    raffleCloseMessage("錯誤", "無法執行抽獎，需先上傳人員名單。");
-  } else if (gifts.length === 0) {
-    raffleCloseMessage("錯誤", "無法執行抽獎，需先上傳獎品清單。");
+    //設置彈出視窗的錯誤訊息
+    if (participants.length === 0 && gifts.length === 0) {
+      errorModalMessageElement.textContent = "需先上傳人員名單和獎品清單。";
+    } else if (participants.length === 0) {
+      errorModalMessageElement.textContent = "需先上傳人員名單。";
+    } else if (gifts.length === 0) {
+      errorModalMessageElement.textContent = "需先上傳獎品清單。";
+    }
+
+    const errorModal = new bootstrap.Modal(errorModalElement); //顯示錯誤彈出視窗
+    errorModal.show();
   } else {
-    performRaffle((index = 0));
-  } //若人員.csv and 獎品.csv都上傳，則執行performraffle函式
+    performRaffle((index = 0)); //若人員.csv and 獎品.csv都上傳，則執行performraffle函式
+  }
 }
 
 function performRaffle(index) {
@@ -176,7 +187,7 @@ function performRaffle(index) {
         i === index ? "yellow" : "";
     }
 
-    //讓視窗跟著游標跑
+    //讓視窗跟著光標跑
     eligibleParticipantsElements[index].scrollIntoView({
       behavior: "smooth",
       block: "center",
@@ -200,7 +211,6 @@ function performRaffle(index) {
     handleStoppedHighlighting(index); // 輸出當前的人員index，並由下一個函式接手
   }, during_time);
 
-  /*
   if (eligibleParticipants.length === 0) {
     // 若沒有未中獎人員，或沒有獎品了，則無法再抽獎
     document.getElementById("result").innerHTML = "沒有更多人可以參加抽獎了！";
@@ -211,7 +221,6 @@ function performRaffle(index) {
     document.getElementById("result").innerHTML = "所有獎品都抽完了！";
     return;
   }
-  */
 
   return index;
 }
@@ -234,6 +243,24 @@ function handleStoppedHighlighting(currentIndex) {
     document.getElementById("winnerModal")
   ); //綁定彈出視窗
   winnerModal.show(); //顯示彈出視窗
+
+  /*
+  winner.hasWon = true; //將該中獎人設為已中獎
+  currentGift.quantity -= 1; //減少當前獎項的數量
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-"); //設定得獎時間標記
+  winners.push({
+    //將winner的資料，加入到winners之中
+    name: winner.name,
+    email: winner.email,
+    department: winner.departmentName,
+    prize: currentGift.name,
+    timestamp: timestamp,
+    
+  });
+  */
+  //updateParticipantWinStatus(participants, winner.email, currentGift.name);
+
+  //document.getElementById("confirmRaffle-button").disabled = false; //抽獎完成後，啟動confirm 按鈕，用做UI流程控制。
 }
 
 function updateParticipantWinStatus(participants, findTheEmailText, prizeName) {
@@ -282,6 +309,20 @@ function downloadResultCSV() {
   downloadCSV(filename, csvContent);
 }
 
+function downloadWinnersCSV() {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-"); //輸出 2023-12-23T19-30-25-000Z 格式的時間戳記
+  const main_title = document.getElementById("main_title").value; //取回網頁上活動標題的內文
+  const filename = `${main_title}_Winners_${timestamp}.csv`; //設定存檔檔名為 活動標題加上時間戳記
+
+  let bom = "\uFEFF"; // UTF-8 的 BOM
+  let csvContent = "data:text/csv;charset=utf-8," + bom; // UTF-8 的 BOM
+  csvContent += "Name,ID,department,Prize,Timestamp\r\n"; // 在CSV的第一行，設置Name,Prize,Timestamp
+  winners.forEach(function (winner) {
+    csvContent += `${winner.name},${winner.email},${winner.department},${winner.prize},${winner.timestamp}\r\n`; //將winners 內容迖代進csvContent之前
+  });
+  downloadCSV(filename, csvContent);
+}
+
 // 等待DOM載入完畢
 document.addEventListener("DOMContentLoaded", (event) => {
   // 通過ID選擇按鈕並綁定click事件
@@ -319,7 +360,7 @@ function confirmRaffle() {
   updateEligibleParticipantsList(); //刷新可抽獎人員/可抽禮物/中獎人員名單
   updateAvailablePrizesList();
   updateWinnersList();
-  checkAndToggleRaffleButton();
+
   document.getElementById("totalWinner").textContent = total_winner; //更新中獎總人數
 }
 
@@ -329,6 +370,24 @@ function cancelRaffle() {
   window.scroll(0, 0);
   updateParticipantWinStatus(participants, winner.email, "give up"); //中獎人放棄
   sortEligibleParticipantsListByRandom();
+}
+
+function downloadNotWinnersCSV() {
+  //操作邏輯同前
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  const main_title = document.getElementById("main_title").value;
+  const filename = `${main_title}_notWinner_${timestamp}.csv`;
+
+  let bom = "\uFEFF";
+  let csvContent = "data:text/csv;charset=utf-8," + bom;
+  csvContent += "Name,id,department,prize,timestamp\r\n"; // CSV头部
+
+  //const notWinners = participants.filter((p) => !p.hasWon);
+  participants.forEach(function (participant) {
+    csvContent += `${participant.name},${participant.email},${participant.departmentName},${participant.hasWon},${participant.timeStamp},\r\n`;
+  });
+
+  downloadCSV(filename, csvContent);
 }
 
 function downloadCSV(filename, csvContent) {
@@ -355,38 +414,38 @@ document
   .getElementById("randomSorting-button")
   .addEventListener("click", sortEligibleParticipantsListByRandom);
 
-// 函數用於查找重複的姓名和信箱
+// 函数用于查找重复的姓名和邮箱
 function findDuplicates() {
-  let nameMap = {}; // 用於記錄名字
-  let emailMap = {}; // 用於記錄信箱
-  let duplicates = { names: [], emails: [] }; // 用於儲存重複的名字和信箱
+  let nameMap = {}; // 用于记录名字
+  let emailMap = {}; // 用于记录邮箱
+  let duplicates = { names: [], emails: [] }; // 用于存储重复的名字和邮箱
 
   participants.forEach((participant) => {
-    // 檢查名字是否重複
+    // 检查名字是否重复
     if (nameMap[participant.name]) {
-      duplicates.names.push(participant); // 如果名字已存在，推入重複名單
+      duplicates.names.push(participant); // 如果名字已存在，推入重复名单
     } else {
-      nameMap[participant.name] = true; // 否則記錄該名字
+      nameMap[participant.name] = true; // 否则记录该名字
     }
 
-    // 檢查信箱是否重複
+    // 检查邮箱是否重复
     if (emailMap[participant.email]) {
-      duplicates.emails.push(participant); // 如果信箱已存在，推入重複名單
+      duplicates.emails.push(participant); // 如果邮箱已存在，推入重复名单
     } else {
-      emailMap[participant.email] = true; // 否則記錄該信箱
+      emailMap[participant.email] = true; // 否则记录该邮箱
     }
   });
 
-  // 顯示重複的名字和信箱
+  // 显示重复的名字和邮箱
   displayDuplicates(duplicates);
 
-  // 返回重複項數組以備進一步處理
+  // 返回重复项数组以备进一步处理
   return duplicates;
 }
 
-// 函數用於在頁面上顯示重複項
+// 函数用于在页面上显示重复项
 function displayDuplicates(duplicates) {
-  // 假設頁面上有兩個列表用於顯示重複的名字和信箱
+  // 假设页面上有两个列表用于显示重复的名字和邮箱
   const nameList = document.getElementById("duplicateNamesList");
   const emailList = document.getElementById("duplicateEmailsList");
 
@@ -394,66 +453,66 @@ function displayDuplicates(duplicates) {
   nameList.innerHTML = "";
   emailList.innerHTML = "";
 
-  // 顯示重複的名字
+  // 显示重复的名字
   duplicates.names.forEach((duplicate) => {
     const listItem = document.createElement("li");
-    listItem.className = "list-group-item"; // 添加 Bootstrap 列表項樣式
+    listItem.className = "list-group-item"; // 添加 Bootstrap 列表项样式
     listItem.textContent = `${duplicate.name}`;
     nameList.appendChild(listItem);
   });
 
-  // 獲取顯示重複姓名數量的徽章元素
+  // 获取显示重复姓名数量的徽章元素
   const duplicateNamesCount = document.getElementById("duplicateNamesCount");
 
   if (duplicates.names.length === 0) {
-    duplicateNamesCount.style.display = "inline-block"; // 使徽章可見
-    // 如果沒有重複項，顯示綠色打勾圖示
+    duplicateNamesCount.style.display = "inline-block"; // 使徽章可见
+    // 如果没有重复项，显示绿色打勾图标
     duplicateNamesCount.className = "badge bg-success rounded-pill";
-    // 使用 Bootstrap Icons 或其他圖示庫的圖示類（如下面範例使用了 Font Awesome）
+    // 使用 Bootstrap Icons 或其他图标库的图标类（如下面示例使用了 Font Awesome）
     duplicateNamesCount.innerHTML =
       '<i class="fa fa-check" aria-hidden="true"></i>';
   } else {
-    // 如果有重複項，顯示紅色徽章並顯示數量
-    duplicateNamesCount.style.display = "inline-block"; // 使徽章可見
+    // 如果有重复项，显示红色徽章并显示数量
+    duplicateNamesCount.style.display = "inline-block"; // 使徽章可见
     duplicateNamesCount.className = "badge bg-danger rounded-pill";
     duplicateNamesCount.textContent = duplicates.names.length;
   }
 
-  // 顯示重複的信箱
+  // 显示重复的邮箱
   duplicates.emails.forEach((duplicate) => {
     const listItem = document.createElement("li");
-    listItem.className = "list-group-item"; // 添加 Bootstrap 列表項樣式
+    listItem.className = "list-group-item"; // 添加 Bootstrap 列表项样式
     listItem.textContent = `${duplicate.email}`;
     emailList.appendChild(listItem);
   });
 
-  // 獲取顯示重複Email數量的徽章元素
+  // 获取显示重复Email数量的徽章元素
   const duplicateEmailCount = document.getElementById("duplicateEmailCount");
 
   if (duplicates.emails.length === 0) {
-    duplicateEmailCount.style.display = "inline-block"; // 使徽章可見
-    // 如果沒有重複項，顯示綠色打勾圖示
+    duplicateEmailCount.style.display = "inline-block"; // 使徽章可见
+    // 如果没有重复项，显示绿色打勾图标
     duplicateEmailCount.className = "badge bg-success rounded-pill";
-    // 使用 Bootstrap Icons 或其他圖示庫的圖示類（如下面範例使用了 Font Awesome）
+    // 使用 Bootstrap Icons 或其他图标库的图标类（如下面示例使用了 Font Awesome）
     duplicateEmailCount.innerHTML =
       '<i class="fa fa-check" aria-hidden="true"></i>';
   } else {
-    // 如果有重複項，顯示紅色徽章並顯示數量
-    duplicateEmailCount.style.display = "inline-block"; // 使徽章可見
+    // 如果有重复项，显示红色徽章并显示数量
+    duplicateEmailCount.style.display = "inline-block"; // 使徽章可见
     duplicateEmailCount.className = "badge bg-danger rounded-pill";
     duplicateEmailCount.textContent = duplicates.emails.length;
   }
 }
 
-// 搜索功能實現
+// 搜索功能实现
 function searchParticipant() {
   var input, filter, ul, li, i, txtValue;
   input = document.getElementById("searchInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("searchResults");
-  ul.innerHTML = ""; // 清空之前的搜索結果
+  ul.innerHTML = ""; // 清空之前的搜索结果
 
-  // 遍歷參與者數據來尋找匹配項
+  // 遍历参与者数据来寻找匹配项
   for (i = 0; i < participants.length; i++) {
     txtValue = participants[i].name;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -522,29 +581,31 @@ function checkInputs() {
   }
 }
 
-function checkAndToggleRaffleButton() {
-  // 檢查是否有可抽獎的人員和禮物
-  const canRaffle =
-    participants.length > 0 && gifts.some((gift) => gift.quantity > 0);
+// 非數字無法輸入在畫面上
+// 獲取輸入框元素
+const drawSecondvalueMax_value = document.getElementById("drawSecondvalueMax");
+const drawSecondvalueMin_value = document.getElementById("drawSecondvalueMin");
 
-  // 獲取開始抽獎按鈕元素
-  const startRaffleButton = document.getElementById("startRaffle-button");
-
-  // 根據條件啟用或禁用開始抽獎按鈕
-  if (canRaffle === false) {
-    startRaffleButton.disabled = true;
+// 添加事件監聽器
+drawSecondvalueMax_value.addEventListener("keydown", function (event) {
+  // 如果輸入的按鍵不是數字，則阻止輸入
+  if (
+    event.key !== "Backspace" &&
+    event.key !== "Delete" &&
+    isNaN(Number(event.key))
+  ) {
+    event.preventDefault();
   }
-}
+});
 
-function raffleCloseMessage(messageTitle, message) {
-  const errorModalElement = document.getElementById("errorModal"); //綁定index.html下的errorModal元件
-  const errorModalElementTitle = document.getElementById("errorModalTitle");
-  const errorModalMessageElement = document.getElementById("errorModalMessage"); //綁定index.html下的errorModalMessage元件
-
-  //設置彈出視窗的錯誤訊息
-  errorModalElementTitle.textContent = messageTitle;
-  errorModalMessageElement.textContent = message;
-
-  const errorModal = new bootstrap.Modal(errorModalElement); //顯示錯誤彈出視窗
-  errorModal.show();
-}
+// 添加事件監聽器
+drawSecondvalueMin_value.addEventListener("keydown", function (event) {
+  // 如果輸入的按鍵不是數字，則阻止輸入
+  if (
+    event.key !== "Backspace" &&
+    event.key !== "Delete" &&
+    isNaN(Number(event.key))
+  ) {
+    event.preventDefault();
+  }
+});
